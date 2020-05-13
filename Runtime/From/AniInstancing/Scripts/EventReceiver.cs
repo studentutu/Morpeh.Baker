7def164f2abda70a7fe4363483cafebb089a0b75
Roman Fedorov <fedorov@plavalaguna.ru>
2020-05-13 18:40:45 +0300

    added AniInstanced System, old scripts (to be removed )

---
+++ Runtime/From/AniInstancing/Scripts/EventReceiver.cs
@@ -0,0 +1,20 @@
+﻿/*
+THIS FILE IS PART OF Animation Instancing PROJECT
+AnimationInstancing.cs - The core part of the Animation Instancing library
+
+©2017 Jin Xiaoyu. All Rights Reserved.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventReceiver : MonoBehaviour
+{
+
+    public void NewEvent(AnimationInstancing.AnimationEvent evt)
+    {
+        //Debug.Log(log);
+        //Debug.Log("PrintEvent: " + evt.stringParameter + " called at: " + Time.time);
+    }
+}
